e3d5a4dc0a999c11461d18dcb5ae0e5e6b82a287
mitya66 <95367065+mitya66@users.noreply.github.com>
2022-02-21 15:38:37 +0500

    Add files via upload

--- project/js/main.js
+++ project/js/main.js
@@ -1,29 +1,107 @@
-'use strict'
+'use strict';
 
-const products = [
-  {id: 1, title: 'Notebook', price: 1000},
-  {id: 2, title: 'Mouse', price: 100},
-  {id: 3, title: 'Keyboard', price: 250},
-  {id: 4, title: 'Gamepad', price: 150},
-];
+class BasketItem {
+    addCartItem(cartItem) {
+      this.goods.push(cartItem);
+    }
+}
 
-const renderProduct = (title, price) => {
-  return `
-      <div class="container">
-          <div class="card">
-              <h3>${title}</h3>
-              <p>${price}</p>
-              <button class="buy-btn">Добавить</button>                     
-          </div>
-      </div>`
-};
+class ProductList {
+    constructor(container = '.products') {
+        this._container = document.querySelector(container);
+        this._goods = [];
+        this._allProducts = [];
 
-const renderProducts = (list) => {
-    const productsList = list
-      .map(item => renderProduct(item.title, item.price, item.image));
-    // console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join(" ");
-};
+        this._fetchGoods();
+        this._render();
+    }
 
-renderProducts(products);
 
+    _fetchGoods() {
+        this._goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keyboard', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ];
+    }
+
+    _render() {
+        for (const product of this._goods) {
+            const productObject = new ProductItem(product);
+
+            this._allProducts.push(productObject);
+            this._container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+        }
+    }
+    
+    createTotal() {
+        const h2Total = document.createElement('h2');
+        h2Total.classList.add('total');
+        const flexRow = document.querySelector('.flex__row');
+        flexRow.append(h2Total);
+        h2Total.innerHTML = 'ИТОГО, общая стоимость товара, составляет: ';
+        }
+    sumTotal() {
+    const sumT = document.querySelector('.total');
+    let sum = 0;
+      for (let product of this.goods) {
+        sum += product.price;
+        }
+    sumT.insertAdjacentHTML('beforeend', `${sum} \u20bd`);
+    }
+
+    sum() {
+        return this._allProducts.reduce((sum, { price }) => sum + price, 0);
+      }
+    }
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.img = img;
+    }
+
+    getHTMLString() {
+        return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <div class="desc">
+                  <h3>${this.title}</h3>
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+              </div>
+          </div>`;
+    }
+}
+new ProductList();
+
+
+
+// const products = [
+//   {id: 1, title: 'Notebook', price: 20000},
+//   {id: 2, title: 'Mouse', price: 1500},
+//   {id: 3, title: 'Keyboard', price: 5000},
+//   {id: 4, title: 'Gamepad', price: 4500},
+// ];
+//
+// const renderProduct = (item, img='https://via.placeholder.com/200x150') => `<div class="product-item" data-id="${this.id}">
+//               <img src="${img}" alt="Some img">
+//               <div class="desc">
+//                   <h3>${item.title}</h3>
+//                   <p>${item.price} \u20bd</p>
+//                   <button class="buy-btn">Купить</button>
+//               </div>
+//           </div>`;
+//
+// const renderProducts = list => {
+//   document.querySelector('.products')
+//       .insertAdjacentHTML(
+//           'beforeend',
+//           list.map(item => renderProduct(item)).join('')
+//       );
+// };
+//
+// renderProducts(products);
+//
